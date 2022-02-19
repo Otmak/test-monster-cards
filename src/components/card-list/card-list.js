5eccf64ff48ed0189af7aad8c446dbeb35e17cb3
otmak <jakelovz@gmail.com>
2022-02-19 02:56:39 -0800

    caro

--- src/components/card-list/card-list.js
+++ src/components/card-list/card-list.js
@@ -45,12 +45,34 @@ export default class CardList extends Component {
   	return(
 		<div id="wrapper">
 			<div id="content">
-	        	<div ref={this.carousel} id="carousel">
+	      <div ref={this.carousel} id="carousel">
 				 	{ this.props.monsters.map( monster => ( 
 				 	<Card key={monster.id} monster={ monster }/> )) 
 				 	} 
 			 	</div>
 		 	</div>
+ 				<button style={{ display: display}} onClick={ this.prev } id="prev">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+        >
+          <path fill="none" d="M0 0h24v24H0V0z" />
+          <path d="M15.61 7.41L14.2 6l-6 6 6 6 1.41-1.41L11.03 12l4.58-4.59z" />
+        </svg>
+      </button>
+      <button onClick={ this.next } id="next">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+        >
+          <path fill="none" d="M0 0h24v24H0V0z" />
+          <path d="M10.02 6L8.61 7.41 13.19 12l-4.58 4.59L10.02 18l6-6-6-6z" />
+        </svg>
+      </button>
 		</div>
 		)
   }
